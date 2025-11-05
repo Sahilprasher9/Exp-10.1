6d94bd16297e0ee888eadd149175f55c90346b57
Sahilprasher9 <prashersahil68@gmail.com>
2025-11-06 00:09:53 +0530

    Create fronted.src.index.js

---
+++ fronted.src.index.js
@@ -0,0 +1,9 @@
+
+```js
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(<App />)
+```
